dd1bb62ef15d988616ca80d20dd5f588a6135572
ryu10 <ryu10nt@gmail.com>
2024-12-07 13:05:26 -0800

    add sbc6809 wip

---
+++ scripts/target/sbc6809/sbc6809.lua
@@ -0,0 +1,17 @@
+STANDALONE = true
+
+CPUS["M6800"] = true
+
+-- MACHINES["Z80DAISY"] = true
+
+function standalone()
+	files{
+		MAME_DIR .. "src/sbc6809/main.cpp",
+		MAME_DIR .. "src/sbc6809/sbc6809.cpp",
+		MAME_DIR .. "src/sbc6809/sbc6809.h",
+		MAME_DIR .. "src/sbc6809/interface.h",
+		MAME_DIR .. "src/sbc6809/osd.h",
+		MAME_DIR .. "src/sbc6809/osd_linux.c",
+	}
+end
+
