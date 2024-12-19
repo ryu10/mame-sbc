67b8410cbd9160e422d7c2a065fa19386b577abe
ryu10 <ryu10nt@gmail.com>
2024-12-18 18:48:27 -0800

    add support for uart_tty on sbc6800

--- src/sbc6800/sbc6800.cpp
+++ src/sbc6800/sbc6800.cpp
@@ -9,11 +9,10 @@
 ******************************************************************************/
 
 #include "emu.h"
-// #include "cpu/z80/z80.h"
 #include "cpu/m6800/m6800.h"
 #include "sbc6800.h"
 #include "interface.h"
-#include "osd.h"
+#include "machine/uart_tty.h"
 
 #include <cstdio>
 #include <cstdlib>
@@ -25,7 +24,8 @@ public:
 		driver_device(mconfig, type, tag),
 		m_maincpu(*this, "maincpu"),
 		m_main_ram(*this, "main_ram"),
-		m_main_rom(*this, "main_rom")
+		m_main_rom(*this, "main_rom"),
+		m_uart(*this, "uart")
 	{
 		fprintf(stderr, "sbc6800_state: constructor\n");
 	}
@@ -34,7 +34,6 @@ public:
 	uint8_t uart_dreg_r();
 	void uart_creg_w(uint8_t data);
 	void uart_dreg_w(uint8_t data);
-	// void display_w(offs_t offset, uint8_t data);
 
 	void m68_mem(address_map &map) ATTR_COLD;
 	// void io_map(address_map &map) ATTR_COLD;
@@ -44,6 +43,7 @@ private:
 	required_device<cpu_device> m_maincpu;
 	required_shared_ptr<uint8_t> m_main_ram;
 	required_shared_ptr<uint8_t> m_main_rom;
+	required_device<uart_device> m_uart;
 
 	virtual void machine_reset() override ATTR_COLD;
 };
@@ -57,65 +57,26 @@ void sbc6800_state::machine_reset()
 {
 
 	// program is self-modifying, so need to refresh it on each run
-	// memcpy(m_main_ram+0xe000, sbc6800_binary, sizeof sbc6800_binary);
 	memcpy(m_main_rom, sbc6800_binary, sizeof sbc6800_binary);
 	fprintf(stderr, "memcpy: %ld bytes to main_rom\n", sizeof sbc6800_binary);
 	fprintf(stderr, "reset vector: %02X %02X \n", m_main_rom[0xfffe - 0xe000], m_main_rom[0xffff - 0xe000]);
-	// serial reset
-	input_device_reset();
-	output_device_reset();
 	fprintf(stderr, "machine_reset\n");
 
+	// m6800 requires reset
 	m_maincpu->set_input_line(INPUT_LINE_RESET, ASSERT_LINE);
 	m_maincpu->set_input_line(INPUT_LINE_RESET, CLEAR_LINE);
 }
-
 
 /******************************************************************************
  I/O Handlers
 ******************************************************************************/
 
-uint8_t sbc6800_state::uart_creg_r()
-{
-	// spit out the byte in out_byte if out_req is not equal to out_req_last
-	uint8_t c;
-
-	output_device_update();
-	update_user_input();
-	c = input_device_status();
-	// c |= 2;
-	// fprintf(stderr, "[%d]", c);
-	return c;
-}
-
-void sbc6800_state::uart_creg_w(uint8_t data)
-{
+uint8_t sbc6800_state::uart_dreg_r() { return m_uart->data_r(); }
+void    sbc6800_state::uart_dreg_w(uint8_t data) { m_uart->data_w(data); }
+uint8_t sbc6800_state::uart_creg_r() {	return m_uart->status_r(); }
+void    sbc6800_state::uart_creg_w(uint8_t data) {
 	// fprintf(stderr, "uart_creg_w: %02x\n", data);
 }
-
-std::uint8_t sbc6800_state::uart_dreg_r()
-{
-	std::uint8_t ch;
-	output_device_update();
-	update_user_input();
-	ch = input_device_read();
-	return ch;
-}
-
-void sbc6800_state::uart_dreg_w(uint8_t data)
-{
-	// if (data < 0x20) {
-    //    fprintf(stderr, "[%02x]", data);
-	// }
-	output_device_write(data);
-	output_device_update();
-	update_user_input();
-}
-
-// void sbc6800_state::display_w(offs_t offset, uint8_t data)
-// {
-// 	fprintf(stderr, "io_w: %04x %02x\n", offset, data);
-// }
 
 /******************************************************************************
  Address Maps
@@ -128,15 +89,6 @@ void sbc6800_state::m68_mem(address_map &map)
 	map(0x8019, 0x8019).rw(FUNC(sbc6800_state::uart_dreg_r), FUNC(sbc6800_state::uart_dreg_w));
 	map(0xe000, 0xffff).rom().share("main_rom");
 }
-
-// void sbc6800_state::io_map(address_map &map)
-// {
-// 	map.unmap_value_high();
-// 	map.global_mask(0xff);
-// 	map(0x20, 0x25).w(FUNC(sbc6800_state::display_w));
-// 
-// }
-
 
 /******************************************************************************
  Input Ports
@@ -153,11 +105,9 @@ INPUT_PORTS_END
 void sbc6800_state::sbc6800(machine_config &config)
 {
 	/* basic machine hardware */
-	//Z80(config, m_maincpu, XTAL(3'579'545));
-	// Z80(config, m_maincpu, XTAL(40'000'000));
 	M6800(config, m_maincpu, XTAL(16'000'000 / 4));
+	UART(config, m_uart, 9600);
 	m_maincpu->set_addrmap(AS_PROGRAM, &sbc6800_state::m68_mem);
-	// m_maincpu->set_addrmap(AS_IO, &sbc6800_state::io_map);
 }
 
 
